0bf35b5ac879e3f1eb0682f315696e0b68d7969d
Jonathan Calmels <jbjcalmels@gmail.com>
2013-10-20 20:55:11 +0200

    Handle big frames slicing

--- packetizer.c
+++ packetizer.c
@@ -11,6 +11,7 @@ static rtvs_packet_t packet;
 
 void Packetizer_init(void)
 {
+        /* XXX: Not really unpredictable */
         srand(time(NULL));
         BZERO_STRUCT(packet);
 
@@ -20,7 +21,8 @@ void Packetizer_init(void)
         packet.ssrc        = rand();
 }
 
-/* TODO Handle non reference frames (0 by default) */
+#define IS_KEYFRAME(x) (!(x->data[0] & 0x01))
+
 int Packetizer_packetize(rtvs_frame_t *frame)
 {
         size_t size;
@@ -29,44 +31,45 @@ int Packetizer_packetize(rtvs_frame_t *frame)
 
         /* Convert the pts to a 90kHz resolution timestamp */
         packet.timestamp = frame->pts * 90;
-        packet.pstart = 1;
+        packet.marker = 0;
         packet.pid = 0;
+        packet.nonref = 0;
 
         /* Frame fits in one packet */
         if (frame->size <= RTP_PAYLOAD_SIZE) {
+                packet.pstart = 1;
                 packet.marker = 1;
                 memcpy(packet.payload.data, frame->data, frame->size);
-                if (Rtp_send(packet, frame->size) < 0)
-                        return (-1);
+                FAIL_ON_NEGATIVE(Rtp_send(packet, frame->size))
                 ++packet.seqnum;
         }
         /* Send partitions separately */
         else {
-                if (frame->partition_num == 1)
-                        goto too_big;
-                packet.marker = 0;
                 for (; packet.pid < frame->partition_num; ++packet.pid) {
+                        packet.pstart = 1;
                         size = frame->partition_size[packet.pid];
                         if (packet.pid == 0) /* First partition, don't forget the frame header */
-                                size += (frame->data[0] & 0x01) ? FRAME_HEADER_SIZE
-                                        : FRAME_HEADER_SIZE + KEYFRAME_HEADER_SIZE;
-                        if (packet.pid == frame->partition_num - 1) /* Last partition */
+                                size += IS_KEYFRAME(frame) ? FRAME_HEADER_SIZE + KEYFRAME_HEADER_SIZE
+                                                           : FRAME_HEADER_SIZE;
+                        /* Still too big for our packet, split it in chunks of size RTP_PAYLOAD_SIZE */
+                        while (size > RTP_PAYLOAD_SIZE) {
+                                memcpy(packet.payload.data, frame->data, RTP_PAYLOAD_SIZE);
+                                frame->data += RTP_PAYLOAD_SIZE;
+                                frame->size -= RTP_PAYLOAD_SIZE;
+                                FAIL_ON_NEGATIVE(Rtp_send(packet, RTP_PAYLOAD_SIZE))
+                                size -= RTP_PAYLOAD_SIZE;
+                                ++packet.seqnum;
+                                packet.pstart = 0;
+                        }
+                        if (packet.pid == frame->partition_num - 1) /* Last partition, last packet */
                                 packet.marker = 1;
-
-                        if (size > RTP_PAYLOAD_SIZE)
-                                goto too_big;
                         memcpy(packet.payload.data, frame->data, size);
                         frame->data += size;
                         frame->size -= size;
-                        if (Rtp_send(packet, size) < 0)
-                                return (-1);
+                        FAIL_ON_NEGATIVE(Rtp_send(packet, size))
                         ++packet.seqnum;
                 }
                 assert(frame->size == 0);
         }
         return (0);
-
-too_big:
-        errno = EMSGSIZE;
-        return (-1);
 }
